46f7f19948da0ed963aa277b0e72490cbf5af29b
Niluka <nelukarulzz@gmail.com>
2022-12-29 10:05:16 +0530

    solves 4 tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require('@nomicfoundation/hardhat-toolbox');
+// require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+// const coinmarketcapkey = process.env.COINMARKETCAP;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.17',
+  solidity: "0.8.17",
+  // gasReporter: {
+  //   enabled: true,
+  //   // outputFile: "gas-report.txt",
+  //   currency: "USD",
+  //   coinmarketcap: coinmarketcapkey,
+  // },
 };
--- test/game4Test.js
+++ test/game4Test.js
@@ -1,21 +1,26 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Game4', function () {
+describe("Game4", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game4');
+    const Game = await ethers.getContractFactory("Game4");
     const game = await Game.deploy();
+    const [signer, signer2] = await ethers.getSigners();
 
-    return { game };
+    return { game, signer, signer2 };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, signer, signer2 } = await loadFixture(
+      deployContractAndSetVariables
+    );
 
     // nested mappings are rough :}
+    await game.connect(signer).write(signer2.address);
 
-    await game.win();
+    await game.connect(signer2).win(signer.address);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
